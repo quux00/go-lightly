ee5ce2f94bdc53bf270633207b0029bc08690f33
Michael Peterson <midpeter444@localhost.localdomain>
2012-12-27 13:00:28 -0500

    Added more go-lightly examples and with-channel-open macro.
    with-channel-open will call (close ...) not (.close ...) on
    its args, so it works with lamina channels.

--- src/thornydev/go_lightly/core.clj
+++ src/thornydev/go_lightly/core.clj
@@ -1,7 +1,9 @@
 (ns thornydev.go-lightly.core
+  (:require [thornydev.go-lightly.macros :refer [with-channel-open]])
   (:use lamina.core)
   (:gen-class))
 
+;; --- [ one ]--- ;;
 (defn boring-one
   "First Pike example with go channels"
   []
@@ -17,6 +19,8 @@
     (println "You're boring, I'm leaving.")
     (future-cancel bfut)))
 
+;; --- [ two ]--- ;;
+
 (defn boring-two []
   (let [ch (channel)]
     (enqueue ch 1)
@@ -25,9 +29,67 @@
 (defn two []
   (boring-two))
 
-(defn -main [& args]
-  (one)
-  (two)
-  (shutdown-agents)
-  )
+;; --- [ three ]--- ;;
 
+(defn boring-three []
+  (let [ch (channel)
+        ft (future (loop [x 0]
+                     (Thread/sleep (rand-int 1000))
+                     (enqueue ch (str "boring! " x))
+                     (recur (inc x))))]
+    [ch ft]))
+
+(defn three []
+  (let [[ch ft] (boring-three)]
+    (dotimes [_ 5]
+      (println "You say:" @(read-channel ch)))
+    (println "You're boring. I'm leaving.")
+    (close ch)
+    (future-cancel ft)))
+
+(defn boring-four []
+  (let [ch (channel)]
+    (future (loop [x 0]
+              (Thread/sleep (rand-int 1000))
+              (enqueue ch (str "boring! " x))
+              (when (not (closed? ch))
+                (recur (inc x)))))
+    ch))
+
+(defn four []
+  (let [ch (boring-four)]
+    (dotimes [_ 5]
+      (println "You say:" @(read-channel ch)))
+    (close ch)
+    (println "You're boring. I'm leaving.")))
+
+(defn boring-five [msg]
+  (let [ch (channel)]
+    (future (loop [x 0]
+              (Thread/sleep (rand-int 1000))
+              (enqueue ch (str "boring! " msg " " x))
+              (when (not (closed? ch))
+                (recur (inc x)))))
+    ch))
+
+(defn five []
+  (let [joe-ch (boring-five "Joe")
+        ann-ch (boring-five "Ann")]
+    (dotimes [_ 5]
+      (println @(read-channel joe-ch))
+      (println @(read-channel ann-ch)))
+    (close joe-ch)
+    (close ann-ch)
+    (println "You're boring. I'm leaving.")))
+
+(defn six []
+  (with-channel-open [joe-ch (boring-five "Joe")
+                      ann-ch (boring-five "Ann")]
+    (dotimes [_ 5]
+      (println @(read-channel joe-ch))
+      (println @(read-channel ann-ch)))
+    (println "You're boring. I'm leaving.")))
+
+(defn -main [& args]
+  (five)
+  (shutdown-agents))
