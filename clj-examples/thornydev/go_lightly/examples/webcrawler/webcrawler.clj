83e553a17b6add127c38c1ad40f5ef56720f3569
Michael <mpeterson2@gmail.com>
2013-01-12 12:29:49 -0500

    Completed go-lighly version of webcrawler, based on Clojure Prog book ex.

---
+++ clj-examples/thornydev/go_lightly/examples/webcrawler/webcrawler.clj
@@ -0,0 +1,236 @@
+(ns thornydev.go-lightly.examples.webcrawler.webcrawler
+  (:require [thornydev.go-lightly.core :as go]
+            [net.cgrand.enlive-html :as enlive]
+            [clojure.java.io :refer [as-url]]
+            [clojure.string :refer [lower-case]])
+  (:import (java.io StringReader IOException)
+           (java.net URL MalformedURLException)
+           (java.util.concurrent TimeUnit CountDownLatch
+                                 LinkedTransferQueue LinkedBlockingQueue)))
+
+;; A simple webcrawler based on the example from the end of Chapter 4
+;; of the O'Reilly Clojure Programming book.  This version attempts
+;; to more heavily use concurrency queues (channels) than the book
+;; example to compare approaches.
+
+;; As with the book example, this web crawler is ill-behaved in not
+;; throttling connections, so use sparingly for short bursts of testing.
+
+;; ------------------------- ;;
+;; ---[ data structures ]--- ;;
+;; ------------------------- ;;
+
+;; a buffered channel to allow a large number of urls to crawl
+(def ^LinkedBlockingQueue url-channel (go/channel Integer/MAX_VALUE))
+
+;; holds maps of word freqs after parsing an individual page
+(def ^LinkedBlockingQueue freqs-channel (go/channel Integer/MAX_VALUE))
+
+;; status channels for the master thread to stop messages onto
+(def ^LinkedTransferQueue crawler-status-channel (go/channel))
+(def ^LinkedTransferQueue freq-reducer-status-channel (go/channel))
+
+;; set of urls already crawled (ensure no dups searched)
+(def crawled-urls (agent #{}))
+
+;; final tally of all word frequencies
+(def word-frequencies (atom {}))
+
+
+;; ------------------- ;;
+;; ---[ functions ]--- ;;
+;; ------------------- ;;
+
+;; ---[ crawler section ]--- ;;
+
+(defn links-from
+  [base-url html]
+  "Returns a lazy-seq of URLs from parsing a web page"
+  (remove nil? (for [link (enlive/select html [:a])]
+                 (when-let [href (-> link :attrs :href)]
+                   (try
+                     (URL. base-url href)
+                     ; ignore bad URLs
+                     (catch MalformedURLException e))))))
+
+(defn words-from
+  [html]
+  "Returns a lazy-seq of words from parsing a web page"
+  (let [chunks (-> html
+                 (enlive/at [:script] nil)
+                 (enlive/select [:body enlive/text-node]))]
+    (->> chunks
+      (mapcat (partial re-seq #"\w+"))
+      (remove (partial re-matches #"\d+"))
+      (map lower-case))))
+
+(defn process-url
+  "Takes a url (as a string) and:
+    Marks the url a crawled (in the crawled-urls agent).
+    Downloads the page, parses the links from it and puts
+     links not already seen onto the main url-channel
+    Counts the word frequencies on that page into a map and puts
+     that map onto the freqs-channel.
+   Returns void."
+  [^String urlstr]
+  (try
+    (send-off crawled-urls conj urlstr)
+    (let [html (enlive/html-resource
+                (StringReader. (slurp urlstr)))
+          links (links-from (as-url urlstr) html)]
+      (doseq [lnk links :let [nu (str lnk)]]
+        ;; only put on todo list if haven't seen yet
+        (when-not (@crawled-urls nu)
+          (.put url-channel nu)))
+      
+      (->> html
+           words-from
+           (reduce (fn [m word] (update-in m [word] (fnil inc 0))) {})
+           doall
+           (.put freqs-channel)))
+    (catch IOException io) ;; ignore Pushback buffer overflows from tag-soup lib
+    (catch InterruptedException intex) ;; forcibly shut down via future-cancel
+    (catch Exception e (println "process-url ERROR: " e))))
+
+(defn crawl
+  "Main loop for crawlers. Should be called as a go-lightly routine/thread.
+   In each loop it checks for a shut down msg on the crawler-status-channel
+   and grabs the CountDownLatch out of that msg and decrements its count."
+  []
+  (try
+    (loop []
+      (process-url (.take url-channel))
+      (let [msg (.poll crawler-status-channel)]
+        (if (nil? msg)
+          (recur)
+          (.countDown (:latch msg)))))
+    (catch Exception e (println "crawl ERROR: " e))))
+
+(defn start-crawlers
+  "Takes a number of crawlers to start and spawns them
+   in go-lightly routines."
+  [ncrawlers]
+  (dotimes [id ncrawlers]
+    (go/go (crawl))))
+
+
+;; ---[ frequency reducer section ]--- ;;
+
+(defn freq-cycle
+  "Grabs the next map value off freq-channel, merges it with the
+   master map (msubtots) and returns an updated map."
+  [msubtots]
+  (if-let [freqs (.poll freqs-channel 100 TimeUnit/MILLISECONDS)]
+      (merge-with + msubtots freqs)
+      msubtots))
+
+(defn drain-freqs-channel
+  "Drains all the remaining values (maps) it can get off the freqs-channel
+   in one swoop and merges those counts into the +msubtots+ map passed in.
+   Returns a revised word frequency map."
+  [msubtots]
+  (->> msubtots
+       (conj (go/drain-to-vec freqs-channel))
+       (apply merge-with +)))
+
+(defn reduce-freqs
+  "The main loop for a word frequency reducer, which reads word freq maps
+   put onto the freqs-channel and reduces them into a single master
+   word master map.  When a stop message comes in on the
+   freq-reducer-status-channel, it grabs whatever is left on the freqs-
+   channel, sets the value on the word-frequencies shared atom and messages
+   back to the main controller with its id, signalling that it has finished."
+  [id]
+  (try
+    (loop [subtots {}]
+      (let [msg (.poll freq-reducer-status-channel)]
+        (if-not (nil? msg)
+          (do (reset! word-frequencies (drain-freqs-channel subtots))
+              (.put (:channel msg) id))
+          (recur (freq-cycle subtots)))))
+    (catch Exception e (println "reduce-freqs ERROR" e))))
+
+(defn start-frequency-reducer
+  "Spawns a go-lightly routine for a single frequency reducer that will process
+   the word frequency maps being put onto the freqs-channel by the crawlers."
+  [id]
+  (go/go (reduce-freqs id)))
+
+
+;; ---[ main control section ]--- ;;
+
+(defn stop-crawlers
+  "Signal all the crawlers to stop. The message to the crawlers includes a
+   CountDownLatch. Wait (with a scaled timeout) for the latch to cound down
+   to zero before proceeding."
+  [ncrawlers]
+  (go/with-timeout (min 2000 (* 600 ncrawlers))
+    (let [latch (CountDownLatch. ncrawlers)]
+      (dotimes [_ ncrawlers]
+        (.put crawler-status-channel {:msg :stop, :latch latch}))
+      (.await latch))))
+
+(defn stop-frequency-reducer
+  "Signal the frequency-reducer to stop. The message to the reducer includes
+   a go-lightly channel for the reducer to message back on when it finishes.
+   This fn waits (with 2 sec timeout) for the reducer to message back."
+  []
+  (go/with-timeout 2000
+    (let [back-channel (go/channel)]
+      (.put freq-reducer-status-channel {:msg :stop
+                                         :channel back-channel})
+      (.take back-channel))))
+
+(defn init
+  "Initialize the data structures. Needed for repeated use in a REPL."
+  [urlstr]
+  (.clear url-channel)
+  (.put url-channel urlstr)
+  (.clear freqs-channel)
+  (.clear freq-reducer-status-channel)
+  (.clear crawler-status-channel)
+  (reset! word-frequencies {})
+  (if (agent-error crawled-urls)
+    (restart-agent crawled-urls #{} :clear-actions true))
+  (send crawled-urls (fn [_] #{}))
+  (when-not (await-for 1000 crawled-urls)
+    (throw (IllegalStateException.
+            "Unable to reinitialize crawled-urls agent"))))
+
+(defn start [ncrawlers]
+  (start-crawlers ncrawlers)
+  (start-frequency-reducer :freq-reducer))
+
+(defn stop [ncrawlers]
+  (stop-crawlers ncrawlers)
+  (stop-frequency-reducer)
+  (go/stop))
+
+(defn report []
+  (println "------------------------------")
+  (println "url-channel:" (.size url-channel))
+  (println "freqs-channel:" (.size freqs-channel))
+  (println "status-channels:" freq-reducer-status-channel)
+  (println "word-frequencies:" (count @word-frequencies))
+  (println "crawled-urls:" (count @crawled-urls))
+  (println "------------------------------"))
+
+(defn parse-args
+  "arg1: number of crawler go threads
+   arg2: duration to run crawling
+   arg3: initial url to crawl
+   All args are optional"
+  [args]
+  [(if (= 3 (count args)) (last args) "http://golang.org/ref/")
+     (or (first args) 1)
+     (or (second args) 2000)])
+
+(defn -main
+  "See parse-args fn for the optional args that can be passed in."
+  [& args]
+  (let [[url ncrawlers duration] (parse-args args)]
+    (init url)
+    (start ncrawlers)
+    (Thread/sleep duration)
+    (stop ncrawlers)
+    (report)))
--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject go-lightly "0.1.1-SNAPSHOT"
-  :description "Replicating Go concurrency features in Clojure"
+  :description "Facilitating Go concurrency features in Clojure"
   :url "https://www.youtube.com/watch?v=f6kdp27TYZs&feature=youtu.be"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[criterium "0.3.1"]]}}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lamina "0.5.0-beta8"]]
+                 [lamina "0.5.0-beta8"]
+                 [enlive "1.0.1"]]
   :source-paths ["src" "clj-examples"]
+  :javac-options ["-target" "1.7"]
   :main thornydev.go-lightly.examples.run-examples)
