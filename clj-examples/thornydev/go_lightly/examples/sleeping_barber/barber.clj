a25c8ed2c4234bc2bd6f80714ea980c17e397542
Michael <mpeterson2@gmail.com>
2013-02-02 01:47:35 -0500

    minor improvements to barber.clj
    barber.clj: Got with-timeout version working. Swapped gox for go.

--- clj-examples/thornydev/go_lightly/examples/sleeping_barber/barber.clj
+++ clj-examples/thornydev/go_lightly/examples/sleeping_barber/barber.clj
@@ -2,11 +2,21 @@
   (:refer-clojure :exclude [peek take])
   (:require [thornydev.go-lightly.core :refer :all]))
 
+;; Implementation based on Go routine implementation
+;; by Alexey Kacayev: https://gist.github.com/4688906
 
-(def ^:dynamic *num-barbers* 5)
+;; This version uses two threads (Go routines) for:
+;;  - one to bring in clients to the shop one at a time
+;;  - one to select between handling a client just entered
+;;        or to handle a barber becoming available
+;; And then one thread/routine per barber while s/he
+;; is cutting hair
+
+(def ^:dynamic *num-barbers* 3)
 (def ^:dynamic *num-hall-seats* 5)
-(def ^:dynamic *cutting-time* 250)
+(def ^:dynamic *cutting-time* 100)
 
+;; ---[ print helper fns ]--- ;;
 
 (defn prf [& vals]
   (let [s (apply str (interpose " " (map #(if (nil? %) "nil" %) vals)))]
@@ -17,22 +27,27 @@
   (print (str ">>" v "<<\n")) (flush)
   v)
 
-(defn client-producer [clients-ch]
+;; ---[ barber shop fns ]--- ;;
+
+(defn client-producer
+  "producer routine/thread that simulates clients coming
+  to the barber shop"
+  [clients-ch]
   (loop [i 0]
-    (Thread/sleep (rand-int (/ *cutting-time* 1.0)))
+    (Thread/sleep (rand-int (/ *cutting-time* 2)))
     (put clients-ch (keyword (str "c" i)))
     (recur (inc i))))
 
 (defn init-barber-vector []
-  (vec
-   (for [i (range *num-barbers*)]
-     (keyword (str "b" i)))))
+  (vec (for [i (range *num-barbers*)]
+         (keyword (str "b" i)))))
 
 (defn cut-hair [barber client finished-ch]
   (prf barber "cuts hair of" client)
   (Thread/sleep *cutting-time*)
-  (prf "!finished cutting hair of" client)
+  (prf "!" barber "finished cutting hair of" client)
   (put finished-ch barber))
+
 
 (defn client-walked-in [client barber-ch shop-state]
   (if (zero? (count (:free-barbers shop-state)))
@@ -46,45 +61,41 @@
 
     (let [barber (first (:free-barbers shop-state))]
       (prf "Client" client "goes to barber" barber)
-      (gox (cut-hair barber client barber-ch))
+      (go (cut-hair barber client barber-ch))
       (update-in shop-state [:free-barbers] #(vec (rest %))))))
+
 
 (defn barber-available [barber barber-ch shop-state]
   (if (> (count (:waiting-clients shop-state)) 0)  ;; (if (seq (:wc ss))
     (let [client (first (:waiting-clients shop-state))]
       (prf
-       (format "Take client %s from room (remaining clients: %d)",
-               client, (dec (count (:waiting-clients shop-state)))))
-      (gox (cut-hair barber client barber-ch))
+       (format "%s takes client %s from room (remaining clients: %d)",
+               barber, client, (dec (count (:waiting-clients shop-state)))))
+      (go (cut-hair barber client barber-ch))
       (update-in shop-state [:waiting-clients] #(vec (rest %))))
 
     (do (prf "Barber" barber "takes a nap.")
         (update-in shop-state [:free-barbers] conj barber))))
 
 (defn barber-shop [clients-ch]
-  (let [barber-ch (channel)]    
+  (let [barber-ch (channel)]
     (loop [shop-state {:free-barbers (init-barber-vector)
-                       :waiting-clients []}]  ;; TODO: should be PersistentQueue
+                       :waiting-clients []}]
       (-> (selectf
            clients-ch #(client-walked-in % barber-ch shop-state)
            barber-ch  #(barber-available % barber-ch shop-state))
-          prv
+          prv  ;; uncomment to see the shop-state after each select
           (recur)))))
 
-;; TODO: why does this with-timeout not work?
-;;       Does it not compose with go or gox?
-(defn -main2 [& args]
+(defn -main
+  "This version has one thread/routine for continuously bringing
+  clients into the shop and another thread that waits for either
+  clients to come in or barbers to become available, hanldes that
+  scenario and repeats."
+  [& args]
   (with-timeout (Integer/valueOf (or (first args) 2000))
     (let [clients-ch (channel)]
-      (gox (client-producer clients-ch))
-      (gox (barber-shop clients-ch))))
-  (prf "done")
-  (stop))
-
-(defn -main [& args]
-  (let [clients-ch (channel)]
-    (gox (client-producer clients-ch))
-    (gox (barber-shop clients-ch)))
-  (Thread/sleep (Integer/valueOf (or (first args) 2000)))
+      (go (client-producer clients-ch))
+      (barber-shop clients-ch)))
   (prf "done")
   (stop))
