be871b5155d1f81f980f207df6b78da65cd580b1
Michael <mpeterson2@gmail.com>
2013-03-04 22:54:17 -0500

    Changed go-lightly Clojure dependency to 1.3.0, not 1.5.0

--- go-lightly/project.clj
+++ go-lightly/project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[criterium "0.3.1"]]}}
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :scm {:name "git"
-        :tag "12f653361a88c4df14"
         :url "https://github.com/midpeter444/go-lightly"})
